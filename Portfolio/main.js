4b1f15468b8e0c49e00d49cd05406b07190ca89b
Immanuel <59102716+emmzzy@users.noreply.github.com>
2021-10-08 13:53:17 +0100

    Add files via upload

---
+++ Portfolio/main.js
@@ -0,0 +1,15 @@
+// select tab content-item
+
+Function selectItem.(e) {
+    removeBorder();
+
+    // add boarder to current tab
+
+    this.classList.add ('tab-border');
+}
+
+function removeBorder(){
+    tabItem.for.Each()
+}
+
+
